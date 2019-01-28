02c035479883a943bda4121c1541a65733aa4289
Jasmine Dahilig <jasmine@dahilig.com>
2019-01-28 12:21:33 -0800

    add basic command line options parsing

--- cake/src/main.rs
+++ cake/src/main.rs
@@ -1,3 +1,16 @@
+extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{}", args.pattern)
 }
